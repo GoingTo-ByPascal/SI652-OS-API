2719f1bbf9435cbcb0e7c3a0ff028125efe7f32a
IPorteniu <lporteniu@gmail.com>
2020-06-05 13:16:02 -0500

    correciones(? sigue sin funcionar

--- src/main/java/goingto/com/model/Country.java
+++ src/main/java/goingto/com/model/Country.java
@@ -33,47 +33,6 @@ public class Country {
     @Column(name = "full_name",length = 100)
     private String fullName;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public void setShortName(String shortName) {
-        this.shortName = shortName;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    
-    public Locatable getLocatable() {
-		return locatable;
-	}
-
-	public void setLocatable(Locatable locatable) {
-		this.locatable = locatable;
-	}
-
-	public Country(Long id, @NotEmpty String shortName, @NotEmpty String fullName, Locatable locatable) {
-		super();
-		this.id = id;
-		this.shortName = shortName;
-		this.fullName = fullName;
-		this.locatable = locatable;
-	}
-
 
 	@OneToOne
 	@JoinColumn(name = "locatable_id")
