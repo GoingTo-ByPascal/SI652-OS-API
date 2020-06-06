b58ba561a7ea9ac26550c9acc6225c55e74110af
Shiohne <Shiohne@github.com>
2020-06-06 13:35:54 -0500

    4 Models Added
    -Achievements
    -Profiles
    -Reviews
    -Tips

---
+++ src/main/java/goingto/com/model/Tips.java
@@ -0,0 +1,25 @@
+package goingto.com.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="tips")
+public class Tips {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotEmpty
+    @Column(name = "text",length = 100)
+    private String text;
+    @ManyToOne
+    @JoinColumn(name = "locatable_id")
+    private Locatable locatable;
+}
