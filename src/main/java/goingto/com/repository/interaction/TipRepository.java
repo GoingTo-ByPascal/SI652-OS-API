36540c41e1533a45c7c279e8961e90c932228551
Shiohne <Shiohne@github.com>
2020-06-23 18:30:01 -0500

    Reviews Fixed

--- src/main/java/goingto/com/repository/interaction/TipRepository.java
+++ src/main/java/goingto/com/repository/interaction/TipRepository.java
@@ -1,6 +1,5 @@
 package goingto.com.repository.interaction;
 
-import goingto.com.model.interaction.Review;
 import goingto.com.model.interaction.Tip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
