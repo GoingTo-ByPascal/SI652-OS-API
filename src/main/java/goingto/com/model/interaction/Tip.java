4925263abc94e577a3368c30339e9157fbb02877
Shiohne <Shiohne@github.com>
2020-06-23 17:41:04 -0500

    BD change in models

--- src/main/java/goingto/com/model/interaction/Tip.java
+++ src/main/java/goingto/com/model/interaction/Tip.java
@@ -2,6 +2,7 @@ package goingto.com.model.interaction;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import goingto.com.model.account.User;
+import goingto.com.model.account.UserProfile;
 import goingto.com.model.geographic.Locatable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -33,8 +34,8 @@ public class Tip {
     private Locatable locatable;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_profile_id", nullable = false)
     @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
-    private User user;
+    private UserProfile userProfile;
 }
