b51150bf06d8ea29ecac57d3dab1ffd2e9778910
Shiohne <Shiohne@github.com>
2020-06-26 21:13:13 -0500

    Test1.0-FIX

--- src/main/java/goingto/com/controller/AchievementController.java
+++ src/main/java/goingto/com/controller/AchievementController.java
@@ -28,7 +28,7 @@ public class AchievementController {
     @ApiOperation("Return all Achievements")
     @GetMapping("/achievements")
     public ResponseEntity<List<Achievement>> getAllAchievements() {
-        List<Achievemet> achievements = new ArrayList<>();
+        List<Achievement> achievements = new ArrayList<>();
         achievements = achievementService.getAllAchievements();
         return ResponseEntity.ok(achievements);
 
